1aad602a91b7bd47f117a623ece8f549543e3b7b
Alex Beattie <artisanbranding@me.com>
2017-09-16 13:47:10 -0700

    added label

--- avenue/ViewController.swift
+++ avenue/ViewController.swift
@@ -19,56 +19,37 @@ class ViewController: UIViewController {
 
 //    var listingDataModel = ListingDataModel()
 
-    var address:String?
-    var remarks:String?
-    var listPrice: Int?
+    @IBOutlet weak var addressLbl: UILabel!
+    @IBOutlet weak var imageOne: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        getData()
+        
+     
+}
+ 
+    func getData() {
         Alamofire.request(URL_LOCAL, method: .get).validate().responseJSON { (response) in
             switch response.result {
             case .success(let value):
                 
                 let json : JSON = JSON(value)
-//                print(self.listings)
                 
-//                if let theSales = json[0]["sales"]["closeDate"] {
-//                 print (theSales)
-//                }
-//
                 if let info = json.arrayObject{
-//                    print ("The fucking json \(info)")
-//                    if let sales = json[0]["sales"]["closeDate"] {
-////                        print(self.labelTest)
-//                    }
-//                    let sales = info["sales"]["closeDate"] as? Dictionary<String,AnyObject>
-//                    print(info)
-//                    if let sales = info["sales"]["closeDate"] {
-//                        print (sales)
-                
-                    //Address
-//                    let address = json[0]["address"]["streetName"]["city"]["state"]["postalCode"]
                     
-                    
-//                    if let streetNumberText = json[1]["address"]["streetNumberText"].string,
-//                     let streetName = json[13]["address"]["streetName"].string?.capitalized,
-//                     let city = json[1]["address"]["city"].string,
-//                     let state = json[1]["address"]["state"].string,
-//                     let postalCode = json[1]["address"]["postalCode"].string {
-////                        print("The state name is \(postalCode)")
-//                        print ("The address is \(streetNumberText) \(streetName) \(city) \(state) \(postalCode)")
-//
-//                                    }
-                    
-//                  // Address/
+                    // Address/
                     guard let full = json[1]["address"]["full"].string?.capitalized else { return }
-                        print(full)
-      
+                    print(full)
+                        self.addressLbl.text = full
+                    
                     //Photos
                     let photos = json[2]["photos"].arrayValue.map({$0.stringValue})
-                        print(photos)
-               
+                    print(photos)
+                        self.imageOne.image = UIImage(named: photos[0])
+                    
                     //Bedrooms
                     if let propertyBedrooms = json[0]["property"]["bedrooms"].int,
                         let propertyBathrooms = json[0]["property"]["bathsFull"].int {
@@ -78,8 +59,8 @@ class ViewController: UIViewController {
                     //Coords
                     let coordlat = json[0]["geo"]["lat"].floatValue
                     let coordslon = json[0]["geo"]["lng"].floatValue
-                        print("The coordinates are \(coordlat),\(coordslon)")
-                  
+                    print("The coordinates are \(coordlat),\(coordslon)")
+                    
                     //Price
                     if let listPrice = json[0]["listPrice"].number  {
                         let largeNumber = listPrice
@@ -87,26 +68,22 @@ class ViewController: UIViewController {
                         numberFormatter.numberStyle = NumberFormatter.Style.decimal
                         
                         if let formattedNumber = numberFormatter.string(from: (largeNumber)) {
-                                print("$\(formattedNumber)")
+                            print("$\(formattedNumber)")
                         }
                     }
-                    
+                    //mlsId
                     if let mlsId = json[0]["mlsId"].int {
                         print("The mlsId is \(mlsId)")
                     }
-             
-
-                
-
-            
+                    print(info)
                 }
             case .failure(let error):
                 print(error)
             }
         }
     }
-}
- 
+        
+    }
 
 
 //Alamofire.request(URL_LOCAL, method: .get).validate().responseJSON { (response) in
